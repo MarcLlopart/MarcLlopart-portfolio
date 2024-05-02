0db929295bb1945f0383353b514b67080fce4ebc
MarcLlopart <markllopart@gmail.com>
2024-05-02 19:09:35 +0200

    add cv and nav bar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,38 +1,61 @@
-
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
-import {getImageUrl} from "../../utils"
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(true); // Set menuOpen to true by default
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-    const [menuOpen, setMenuOpen] = useState(false);
+    useEffect(() => {
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 830); // Update isSmallScreen based on window width
+        };
 
-    return <nav className={styles.navbar}>
-        <a className={styles.title} href="/">Home</a>
-        <div className={styles.menu}>
-            <img 
-            className={styles.menuBtn} 
-            src={menuOpen 
-                ? getImageUrl("nav/closeIcon.png")
-                : getImageUrl("nav/menuIcon.png")}
-            alt="menu-button"
-            onClick={()=> setMenuOpen(!menuOpen)}
-            />
-            <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-            onClick={()=> setMenuOpen(false)}>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#experience">Experience</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
+        handleResize(); // Initial check
+        window.addEventListener("resize", handleResize); // Listen for window resize events
+
+        return () => {
+            window.removeEventListener("resize", handleResize); // Clean up event listener on unmount
+        };
+    }, []);
+
+    return (
+        <nav className={styles.navbar}>
+            <a className={styles.title} href="/MarcLlopart-portfolio/">
+                Home
+            </a>
+            <div className={styles.menu}>
+                {isSmallScreen && ( // Render menu button only on small screens
+                    <img
+                        className={styles.menuBtn}
+                        width={45}
+                        height={45}
+                        src={menuOpen ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")}
+                        alt={menuOpen ? "close-menu-button" : "menu-button"}
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    />
+                )}
+                <ul
+                    className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick={() => setMenuOpen(false)}
+                >
+                    <li>
+                        <a href="#about">About</a>
                     </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </nav>;
-}
+                    <li>
+                        <a href="#experience">Experience</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                    <li>
+                        <a href="assets/cv/cv_mllopart2024.pdf" download>My CV!</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
